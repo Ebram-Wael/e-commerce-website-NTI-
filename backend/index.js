a683370b22c4e3253fca26737abab8c1caaf52dc
Ebram-Wael <202227393@std.sci.cu.edu.eg>
2025-09-02 22:17:56 +0300

    init commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
+});
