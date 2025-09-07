3be0bd52625785619da9bbb514b02a1e2fbd9df8
Ebram-Wael <202227393@std.sci.cu.edu.eg>
2025-09-07 17:20:22 +0300

    fix some errors and typo

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+import usersRoutes from "./Routes/user.route.js";
+import productRoutes from "./Routes/product.route.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.use("/users", usersRoutes);
+app.use("/products", productRoutes);
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
